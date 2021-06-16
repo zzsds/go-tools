29c71ffdf1158c1af85bd276e11617e7f8b58066
wuhaurou <wobusbz2019@qq.com>
2021-06-16 14:53:09 +0800

    新增post请求头

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-24 11:32:44
+ * @LastEditTime: 2021-06-16 14:52:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \socloud-servere:\wuhuarou\renlanglang\zzsd\tools\pkg\client\client.go
+ */
 package client
 
 import (
@@ -13,11 +21,13 @@ const (
 	ContentType = "Content-Type"
 	ContentJson = "application/json"
 	ContentFrom = "application/x-www-form-urlencoded;charset=utf-8"
+	User_Agent  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586"
+	Accept      = "application/x-www-form-urlencoded;charset=utf-8"
 )
 
 type Client struct {
-	hc      *http.Client
-	req     *http.Request
+	hc  *http.Client
+	req *http.Request
 }
 
 func NewClient(ctx context.Context, opts ...OptionClient) (client *Client) {
@@ -45,8 +55,8 @@ func (client *Client) Post(ctx context.Context, requestURL string, requestBody i
 	return client.do(ctx, http.MethodPost, requestURL, requestBody)
 }
 
-func (Client *Client) PostForm(ctx context.Context, requestURL string, requestBody string) (*http.Response, error){
-	 return Client.doRequest(ctx, http.MethodPost, requestURL,ContentFrom, requestBody)
+func (Client *Client) PostForm(ctx context.Context, requestURL string, requestBody string) (*http.Response, error) {
+	return Client.doRequest(ctx, http.MethodPost, requestURL, ContentFrom, requestBody)
 }
 
 func (client *Client) Delete(ctx context.Context, requestURL string) (*http.Response, error) {
@@ -71,6 +81,8 @@ func (client *Client) doRequest(ctx context.Context, method, requestURL, content
 		return nil, err
 	}
 	client.req.Header.Add(ContentType, contentType)
+	client.req.Header.Add("Accept", Accept)
+	client.req.Header.Add("User-Agent", User_Agent)
 	return client.hc.Do(client.req)
 }
 
